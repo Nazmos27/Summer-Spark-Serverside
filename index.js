abb92ba167e3ce2ea67963aab1f5620df3855df7
Nazmos27 <nazmos789@gmail.com>
2023-06-11 00:23:20 +0600

    installed jwt and created token

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const jwt = require('jsonwebtoken')
 const port = process.env.PORT || 5000
 require('dotenv').config()
 // console.log(process.env.USER_NAME);
-// 
-// nazmos_12
+
 // midleware
 app.use(cors())
 app.use(express.json())
@@ -39,7 +39,13 @@ async function run() {
 
 
 
+//json webtoken related APIs
 
+app.post('/jwt',(req,res) => {
+    const user = req.body
+    const token = jwt.sign(user, process.env.ACCESS_TOKEN , {expiresIn: '1h'})
+    res.send({token})
+})
 
 
 //user related API
